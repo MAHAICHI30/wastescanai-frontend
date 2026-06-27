60f6304612fb1a3b0728904017f8b8ff477dbd8c
MAHAICHI30 <aichimah03@gmail.com>
2026-06-27 23:58:18 +0800

    Update scan.php

--- scan.php
+++ scan.php
@@ -23,11 +23,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['waste_image'])) {
     header('Content-Type: application/json');
     
     // 🔌 【核心整合】：直接将数据库配置注入头部，不再依赖外部 include 'db.php';
-    $host = "localhost";
-    $db_user = "root";          // XAMPP 默认数据库用户名
-    $db_pass = "";              // XAMPP 默认数据库密码
-    $db_name = "wastescanaidb"; // 你的数据库名字
-
+    $host = 'mysql.railway.internal';
+    $dbname = 'railway';
+    $user = 'root';
+    $pass = 'VpUQTVAAjVaDLhqBcUZMfxoJhHEpPRKx'; 
+   
     // 建立 MySQL 数据库连接
     $conn = new mysqli($host, $db_user, $db_pass, $db_name);
     
