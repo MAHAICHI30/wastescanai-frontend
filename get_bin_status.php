17190c43ff9a2582dbed4ad405bc8bbe3ce10e9e
MAHAICHI30 <aichimah03@gmail.com>
2026-06-29 01:25:35 +0800

    Update get_bin_status.php

--- get_bin_status.php
+++ get_bin_status.php
@@ -1,12 +1,12 @@
 <?php
 header('Content-Type: application/json');
 
-$host = 'mysql.railway.internal';
-$dbname = 'railway';
-$user = 'root';
-$pass = 'VpUQTVAAjVaDLhqBcUZMfxoJhHEpPRKx'; 
+$servername = "localhost";
+$username = "root";
+$password = ""; 
+$dbname = "wastescanaidb";
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     echo json_encode(["success" => false, "message" => "Database connection failed"]);
